0b4c8c51a87913a87260b03d899970c8cd8498f7
YanFan Hw <yanfanhw@gmail.com>
2022-05-14 12:06:32 +0700

    Design Heading

--- 2048clone/assets/Script/welcome.js
+++ 2048clone/assets/Script/welcome.js
@@ -10,15 +10,22 @@ cc.Class({
             default: null,
             type: cc.Node
         },
+        heading: {
+            default: null,
+            type: cc.Node
+        },
         _text: '2048'
     },
 
     // use this for initialization
     onLoad: function () {
+        this.background.active = false;
+
         cc.tween(this.label.node)
             .to(2, { opacity: 0 })
             .call(() => {
                 this.background.color = new cc.Color(255, 255, 255);
+                // this.background.active = false;
             })
             .start()
         this.label.string = this._text;
